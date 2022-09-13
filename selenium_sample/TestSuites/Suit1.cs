82cbad1c3ca241306f0521a157eb878e87919573
Илья Шехурдин <sheilyand@MacBook-Pro-Ila.local>
2022-09-13 16:19:43 +0300

    Добавил шаблон и шпаргалку

---
+++ selenium_sample/TestSuites/Suit1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace selenium_sample;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
